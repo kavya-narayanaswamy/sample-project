a587d094aafc558031225b621334a59cb9d16163
kavya-narayanaswamy <kavyanarayanaswamy2000@gmail.com>
2024-03-18 16:41:10 +0530

    first commit

---
+++ basic.js/basic.js
@@ -0,0 +1,111 @@
+ /* splice =  let mynumbers = [0, 1,2,3,4,5,6,7]
+  let newNumbers = mynumbers.splice(4,2," somthing to see")
+  document.write(mynumbers + "<br>")
+  document.write(newNumbers)*/
+
+
+ /*indexOf  returs the index at which a given element can be found in a array or -1 if it is not present
+ let students = [ "kavya","banti","gagana","nanni"] 
+ document.write(students.indexOf("nikki"))
+ let score =[ "banti","sam", " sangi","sahana" ]
+ let newStudensts = students.indexOf("chinna")
+ if(newStudensts ===-1){ 
+    students.push("chinna")
+
+ } document.write(students)
+ console.log(students)
+
+
+ /*forEach = "excute a provided funtion once for each array element"
+
+ let  score = [ 10,20,30,40,50];
+ let students = [ " kavya"," nanni"," mani", " chinna"];
+ students.forEach(function(item, index)
+ {
+  document.write(index+1+item+"<br>")
+ })
+  score.forEach(function(sang)
+  {document.write(sang*2+"<br>")})
+  
+ 
+    /*Multi dimentional array
+      let students=[ "kavya", "chinna", "sahana"];
+     let kanna= [ 
+        [
+            ["kanna","chinna"," nanni"],
+            ["sanju","sangi","sahana"]
+        ],
+        [10,20,30,40,50],
+      [5,2,5,9,5],
+      [25,26,27,28],]
+     
+      document.write( "im expecting resul:" + kanna[0][0][2])
+
+
+      STRING METHOD
+
+      let sample = " this is a split method text"
+      let sample ="this is a split method"
+    document.write(sample.split(" "))*/
+
+   /*loops method
+    for(var a=0; a <=12;a++){
+      document.write(a +")"+"i love india"+"<br>")
+    }
+    while method
+  var x=0;
+  while(x<=10){
+    document.write(" i love my nation"+"<br>")
+    x++
+  }
+   
+let y=0;
+do{
+  document.write("this is do whileout" + "<br>")
+
+  y++ 
+
+}while(y>=20)
+ var amount=100
+ var days = 60
+ var interest=2
+ for(i=1; i<=days;i++){
+  if(i%4===0){
+    amount+=interest
+  }
+  document.write("your amount for"+i+"th"+amount+"<br>")
+ }
+   function kanna(){
+    document.write("apple is a red"+"<br>")
+    document.write("this is a kavya"+"<br>")
+   }
+kanna()
+
+var a=5
+var b=10 
+var c=20
+function numbers(){
+
+  document.write(a+"<br>")
+  document.write(b+"<br>")
+  document.write(c+"<br>")
+}
+numbers()
+
+function kavya( a,b){
+
+return a+b
+
+}
+document.write(kavya(10,20)+
+"<br>")
+document.write(kavya(500,80))
+kavya(10,20)
+kavya(500,80)
+let y=0;
+do{
+  document.write("this is a do while out")
+  y++
+}
+while(y<20);*/
+
